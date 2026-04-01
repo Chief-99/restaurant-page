2ed33f03da7a85d992d64a53e1c3d81a65050094
Similo <171865838+Chief-99@users.noreply.github.com>
2026-04-01 16:24:37 +0200

    add:Import the shelf image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './styles.css';
 import bakeryBuilding from './images/bakery-building.svg';
+import bakeryShelf from './images/bakery-shelf.svg';
 
 let bakery = document.getElementById('bakery-img');
+let shelf = document.getElementById('shelf-img');
 bakery.src = bakeryBuilding;
+shelf.src = bakeryShelf;
